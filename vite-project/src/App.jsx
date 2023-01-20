ceaf6e5b293e510958cc550f2255857cfefb9dbe
Rutherford Alexis <rutherford.aalexis@Gmail.com>
2023-01-20 17:02:17 -0500

    channel bar created

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,12 @@
 import Sidebar from "./components/Sidebar"
-
+import ChannelBar from "./components/channelbar"
+import ContentContainer from "./components/ContentContainer"
 function App() {
 
   return (
     <div className="flex">
       <Sidebar />
+      <ChannelBar />
     </div>
   )
 }
