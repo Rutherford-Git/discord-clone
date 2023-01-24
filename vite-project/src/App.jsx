930e04a3fa34d601a08c63b5f039c4baa4d5c1f0
Rutherford Alexis <rutherford.aalexis@Gmail.com>
2023-01-23 20:48:19 -0500

    code cleanup

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,14 @@
-import Sidebar from "./components/Sidebar"
-import ChannelBar from "./components/channelbar"
-import ContentContainer from "./components/ContentContainer"
+import Sidebar from "./components/Sidebar";
+import ChannelBar from "./components/channelbar";
+import ContentContainer from "./components/ContentContainer";
 function App() {
-
   return (
     <div className="flex">
-                    <Sidebar />
+      <Sidebar />
       <ChannelBar />
       <ContentContainer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
